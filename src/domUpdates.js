fd8949797416f76e97ef275b7000d4d5da702a27
Mary Bruff <marybruff5@gmail.com>
2023-10-16 12:02:38 -0600

    minor syntax fixes

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,21 +1,21 @@
-// //NOTE: Your DOM manipulation will occur in this file
-const { userSample } = require('./data/sampleData');
-const { getRandomUser, getUserData, getAverageStepGoal} = require('./scripts');
+const { userSample } = require("./data/sampleData");
+const {
+  getRandomUser,
+  getUserData,
+  getAverageStepGoal,
+} = require("./data-model.js");
 
-
-const userName = document.querySelector('#username');
-const location = document.querySelector('#location');
-const hello = document.querySelector('#progressRightSide');
-const welcomeBack = document.querySelector('#welcomeBack');
-const steps = document.querySelector('#totalSteps');
-const usersAvgSteps = document.querySelector('#averageUsersSteps')
+const userName = document.querySelector("#username");
+const location = document.querySelector("#location");
+const hello = document.querySelector("#progressRightSide");
+const welcomeBack = document.querySelector("#welcomeBack");
+const steps = document.querySelector("#totalSteps");
+const usersAvgSteps = document.querySelector("#averageUsersSteps");
 
 export function displayUserInfo(currentUser, averageSteps) {
   userName.innerText = `${currentUser.name}`;
   location.innerText = `${currentUser.address}`;
-  welcomeBack.innerText = `Welcome Back, ${currentUser.firstName}!`
+  welcomeBack.innerText = `Welcome Back, ${currentUser.firstName}!`;
   steps.innerText = `Step Goal: ${currentUser.dailyStepGoal}`;
   usersAvgSteps.innerText = `The average users step goal: ${averageSteps}`;
 }
-
-
