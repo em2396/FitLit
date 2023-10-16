60fbf74b2996a0738bea04a2ff25ce53ec312c35
Mary Bruff <marybruff5@gmail.com>
2023-10-16 09:18:10 -0600

    fix average users step goal to log on DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,6 @@
 // //NOTE: Your DOM manipulation will occur in this file
 const { userSample } = require('./data/sampleData');
-const { getRandomUser, getUserData, average} = require('./scripts');
+const { getRandomUser, getUserData, getAverageStepGoal} = require('./scripts');
 
 
 const userName = document.querySelector('#username');
@@ -10,12 +10,12 @@ const welcomeBack = document.querySelector('#welcomeBack');
 const steps = document.querySelector('#totalSteps');
 const usersAvgSteps = document.querySelector('#averageUsersSteps')
 
-export function displayUserInfo(currentUser) {
+export function displayUserInfo(currentUser, averageSteps) {
   userName.innerText = `${currentUser.name}`;
   location.innerText = `${currentUser.address}`;
   welcomeBack.innerText = `Welcome Back, ${currentUser.firstName}!`
   steps.innerText = `Step Goal: ${currentUser.dailyStepGoal}`;
-  usersAvgSteps = `The average users step goal: ${average}`
+  usersAvgSteps.innerText = `The average users step goal: ${averageSteps}`;
 }
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,7 @@ import { displayUserInfo } from './domUpdates.js';
 import   sampleData   from './data/sampleData';
 import './styles.css';
 
-//QuerySelectors Here:
-const userName = document.querySelector('#username');
-const location = document.querySelector('#location');
-const hello = document.querySelector('#progressRightSide');
-
-//Variables Here:
+// Global Variables Here:
 let userData = sampleData.sampleUsers;
 let welcome;
 
@@ -15,16 +10,12 @@ let welcome;
 window.addEventListener('load', function() {
   let randomIndex = getRandomUser(userData);
   let currentUser = getUserData(userData, randomIndex);
+  const averageSteps = getAverageStepGoal(userData);
+  displayUserInfo(currentUser, averageSteps); 
+  displayUserInfo(currentUser, averageSteps);
   getAverageStepGoal(userData);
-  displayUserInfo(currentUser);
-  //As a user, I should be able to view an info card with all of my info on the page 
-  //As a user, I should be able to see my first name somewhere prominently on the page to welcome me X
-  //As a user, I should be able to see how my step goal compares to the average step goal amongst all users (this display should not be hard-coded)
 });
-// console.log(getAverageStepGoal(userSample))
-// window.addEventListener('click', function(){
-//   console.log("hello hello helloooooooo")})
-  
+
 //Functions Here:
 function getRandomUser(userData) {
   const currentUserIndex = Math.floor(Math.random() * userData.length);
@@ -41,8 +32,8 @@ function getUserData(userObj, indexPosition) {
   };
 
 function getAverageStepGoal(userSample) {
-    const total = userSample.reduce((accum,user) => {
-        return accum += user.dailyStepGoal;
+    const total = userSample.reduce((acc,user) => {
+        return acc += user.dailyStepGoal;
     }, 0)
     let average = (total / userSample.length).toFixed(0);
     console.log(average)
