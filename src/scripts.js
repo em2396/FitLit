580e61a6df06a44df3c48bf9f114fa3b9f95db02
Emalee Poellot <emalee.poellot@gmail.com>
2023-10-15 22:14:21 -0600

    feat/Clean up Js, remove console logs, add semi colons, add functionality for first name

--- src/scripts.js
+++ src/scripts.js
@@ -2,26 +2,56 @@ import { displayUserInfo } from './domUpdates.js';
 import   sampleData   from './data/sampleData';
 import './styles.css';
 
-
+//QuerySelectors Here:
 const userName = document.querySelector('#username');
 const location = document.querySelector('#location');
-const hello = document.querySelector('#progressRightSide')
+const hello = document.querySelector('#progressRightSide');
 
+//Variables Here:
+let userData = sampleData.sampleUsers;
+let welcome;
 
-
-window.addEventListener('click', function(){
-  console.log("hello hello helloooooooo")})
+//Event Listeners Here: 
+window.addEventListener('load', function() {
+  let randomIndex = getRandomUser(userData);
+  let currentUser = getUserData(userData, randomIndex);
+  getAverageStepGoal(userData);
+  displayUserInfo(currentUser);
+  //As a user, I should be able to view an info card with all of my info on the page 
+  //As a user, I should be able to see my first name somewhere prominently on the page to welcome me X
+  //As a user, I should be able to see how my step goal compares to the average step goal amongst all users (this display should not be hard-coded)
+});
+// console.log(getAverageStepGoal(userSample))
+// window.addEventListener('click', function(){
+//   console.log("hello hello helloooooooo")})
   
-  let userData = sampleData.sampleUsers;
-  
-
-console.log(userData)
-
-
-
-
+//Functions Here:
 function getRandomUser(userData) {
-  console.log(userData)
   const currentUserIndex = Math.floor(Math.random() * userData.length);
     return currentUserIndex;
   };
+
+function getUserData(userObj, indexPosition) {
+   let currentUser = userObj.find((user) => {
+        return user.id === (indexPosition + 1);
+    });
+    const first = currentUser.name.split(' ');
+    currentUser.firstName = first[0];
+    return currentUser;
+  };
+
+function getAverageStepGoal(userSample) {
+    const total = userSample.reduce((accum,user) => {
+        return accum += user.dailyStepGoal;
+    }, 0)
+    let average = (total / userSample.length).toFixed(0);
+    console.log(average)
+    return average; 
+};
+
+  module.exports = {
+      getRandomUser,
+      getUserData,
+      getAverageStepGoal, 
+      average
+    };
