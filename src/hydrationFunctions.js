95e05744b589be3a5a637fd67008d4b4d8faee13
Mary Bruff <marybruff5@gmail.com>
2023-10-18 12:47:53 -0600

    add 0 return to pass sad path test to getOuncesPerDay

--- src/hydrationFunctions.js
+++ src/hydrationFunctions.js
@@ -1,24 +1,46 @@
 //return user's average fluid ounces consumed per day for all time:
+// export function getAvgDailyOunces(id, dataList) {
+//   const userHydrationData = dataList.filter(userObj => userObj.userID === id);
+//   const totalOunces = userHydrationData.reduce((accum, userObj) => {
+//     return accum + userObj.numOunces;
+//   }, 0);
+//   const hydrationAverage = Math.round(totalOunces / userHydrationData.length);
+//   return hydrationAverage;
+// };
+
+//❗👇 ===============================================================>>>>>>>>
+  ///add 0 return to pass sad path test 
 export function getAvgDailyOunces(id, dataList) {
   const userHydrationData = dataList.filter(userObj => userObj.userID === id);
+  
+  if (userHydrationData.length === 0) {
+    return 0; 
+  }
+  
   const totalOunces = userHydrationData.reduce((accum, userObj) => {
     return accum + userObj.numOunces;
   }, 0);
   const hydrationAverage = Math.round(totalOunces / userHydrationData.length);
   return hydrationAverage;
 };
+// ❗ ===============================================================<<<<<<<
+
 
 // Return the user’s fluid ounces they consumed for a specific day:
 export function getOuncesPerDay(userObj, dataList, date) {
-    const hydrationUserId = dataList.find((dataObj) => dataObj.userID === userObj.id && dataObj.date === date)
-    console.log("ounces",hydrationUserId.numOunces)
-    return hydrationUserId.numOunces
+  const hydrationUserId = dataList.find((dataObj) => dataObj.userID === userObj.id && dataObj.date === date);
+  if (hydrationUserId) {
+      console.log("ounces", hydrationUserId.numOunces);
+      return hydrationUserId.numOunces;
+  } else {
+      return 0;
+  }
 };
+
 
 //get ounces each day for all the week (7 days)
 // Return how many fluid ounces of water a user consumed each day over the course of a week (7 days)
 //return total for the week
-
 export function getDataPerWeek(filteredData, startDate) {
     const total = filteredData.sort((a, b) => new Date(a.date) - new Date(b.date))
     let findStartDateIndex = total.findIndex((data) => data.date === startDate)
