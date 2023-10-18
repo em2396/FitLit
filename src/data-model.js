6955b003c3a2eb4b26aca9d6c5c6c0f8c91b58e8
Mary Bruff <marybruff5@gmail.com>
2023-10-18 16:38:27 -0600

    make sure sleep functions match main for Ben to work on

--- src/data-model.js
+++ src/data-model.js
@@ -59,7 +59,25 @@ export function averageSleepQuality(filterUser) {
   return averageSleepQuality;
 };
 
-// Return how many hours a user slept for a specific day
+//Return the user’s average number of hours slept per day
+export function averageSleepDay(filterUser) {
+  const total = filterUser.reduce((acc, user) => {
+    return acc += user.hoursSlept;
+  }, 0)
+  const averageSleep = (total / filterUser.length).toFixed(0);
+  return averageSleep;
+};
+
+//Return the user’s average sleep quality per day over all - Ben started
+export function averageSleepQuality(filterUser) {
+  const total = filterUser.reduce((acc, user) => {
+    return acc += user.sleepQuality;
+  }, 0)
+  const averageSleepQuality = (total / filterUser.length).toFixed(0);
+  return averageSleepQuality;
+};
+
+//Return how many hours a user slept for a specific day
 export function specificSleepDay(filterUser, dateOfSleep) {
   const findDaySlept = filterUser.find(user => {
     return user.date === dateOfSleep
@@ -68,15 +86,13 @@ export function specificSleepDay(filterUser, dateOfSleep) {
   const hoursOnDay = findDaySlept.hoursSlept;
   const string = `Slept for ${hoursOnDay} hours on ${dateOfSleep}`;
   return string;
-  
 }
 
 //Return a user’s sleep quality for a specific day
-export function getUserSleepQuality(filterSleepData, dateOfSleep) {
-  const elementDate = filterSleepData.find((element) => element.date === dateOfSleep)
-  return elementDate.sleepQuality
-}
-
+// export function getUserSleepQuality(filterSleepData, dateOfSleep) {
+//   const elementDate = filterSleepData.find((element) => element.date === dateOfSleep)
+//   return elementDate.sleepQuality
+// }
 
 // Return a user’s sleep quality for each day over the course of a given week (7 days) 
 //Return how many hours a user slept each day over the course of a given week (7 days) - getDataPerWeek(filteredData, startDate) 
