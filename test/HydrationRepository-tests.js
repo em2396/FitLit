b0fc43927303497d31c24bc1c277d9b59a310e7c
Mary Bruff <marybruff5@gmail.com>
2023-10-19 09:45:39 -0600

    fix white space

--- test/HydrationRepository-tests.js
+++ test/HydrationRepository-tests.js
@@ -1,12 +1,18 @@
 import { expect } from "chai";
 
-import { getAvgDailyOunces, getOuncesPerDay, getDataPerWeek, getLatestData } from '../src/hydrationFunctions';
+import {
+  getAvgDailyOunces,
+  getOuncesPerDay,
+  getDataPerWeek,
+  getLatestData,
+} from "../src/hydrationFunctions";
+
 import userSample from "../src/data/sampleData";
 
-describe('HydrationTest', () => {
+describe("HydrationTest", () => {
   let hydrationData;
 
-  beforeEach('initialize hydrationData', () => {
+  beforeEach("initialize hydrationData", () => {
     hydrationData = userSample.hydration;
   });
 
@@ -16,38 +22,37 @@ describe('HydrationTest', () => {
   });
 
   // === happy path === //
-  it('should return 0 if no user data is found', () => {
+  it("should return 0 if no user data is found", () => {
     const testDataList = [];
-    const userId = 1; 
+    const userId = 1;
     const result = getAvgDailyOunces(userId, testDataList);
 
     expect(result).to.equal(0);
   });
 
-// === happy path === //
+  // === happy path === //
   it("should return a number for the amount of ounces a user has consumed on a specific day", () => {
-    const userObj = userSample.sampleUsers[0]; 
-    const specificDate = '2023/03/24'; 
-    const result = getOuncesPerDay(userObj, hydrationData, specificDate);
-
-    expect(result).to.be.a('number'); 
-  });
-
-// === sad path === //
-    it("should return 0 if no user data is found for that date", () => {
     const userObj = userSample.sampleUsers[0];
-    const specificDate = '2023/03/25'; 
+    const specificDate = "2023/03/24";
     const result = getOuncesPerDay(userObj, hydrationData, specificDate);
 
-    expect(result).to.equal(0); 
+    expect(result).to.be.a("number");
   });
-  
-// === happy path === //
+
+  // === sad path === //
+  it("should return 0 if no user data is found for that date", () => {
+    const userObj = userSample.sampleUsers[0];
+    const specificDate = "2023/03/25";
+    const result = getOuncesPerDay(userObj, hydrationData, specificDate);
+
+    expect(result).to.equal(0);
+  });
+
+  // === happy path === //
   it("should return an array with ounces of water for the last 7 days of data", () => {
-    const startDate = '2023/03/25'; 
+    const startDate = "2023/03/25";
     const result = getDataPerWeek(hydrationData, startDate);
 
-    expect(result).to.be.an('array'); 
+    expect(result).to.be.an("array");
   });
-
 });
