a32f19fa6afd52dcd6fefc7ccc68557c10214b6a
Mary Bruff <marybruff5@gmail.com>
2023-10-19 09:44:46 -0600

    fix white space

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -4,8 +4,6 @@ import { getRandomUser, getUserData } from "../src/data-model.js";
 
 import userSample from "../src/data/sampleData";
 
-
-
 describe("getRandomUser", () => {
   let users = userSample.sampleUsers;
 
@@ -18,8 +16,8 @@ describe("getRandomUser", () => {
     expect(randomIndex).to.be.a("number");
     expect(randomIndex).to.be.at.least(0);
     expect(randomIndex).to.be.at.most(users.length - 1);
-  });  
-// === happy path === //
+  });
+  // === happy path === //
   it("should return a random user object from the array", () => {
     const randomIndex = getRandomUser(users);
     const randomUser = userSample.sampleUsers[randomIndex];
@@ -28,33 +26,32 @@ describe("getRandomUser", () => {
 });
 
 // === happy path === //
-describe('getUserData', () => {
-  it('should return user data based on index position', () => {
-    const indexPosition = 1; 
+describe("getUserData", () => {
+  it("should return user data based on index position", () => {
+    const indexPosition = 1;
     const userData = getUserData(userSample.sampleUsers, indexPosition);
     const expectedUserData = {
       id: 2,
-      name: 'Tyreek VonRueden',
-      address: '623 Koelpin Skyway, Lake Luigichester MN 77576-1678',
-      email: 'Nicolette_Halvorson43@yahoo.com',
+      name: "Tyreek VonRueden",
+      address: "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      email: "Nicolette_Halvorson43@yahoo.com",
       strideLength: 4.5,
       dailyStepGoal: 9000,
       friends: [13, 19, 3],
-      firstName: 'Tyreek',
+      firstName: "Tyreek",
     };
     expect(userData).to.deep.equal(expectedUserData);
   });
-// === sad path === //
-  it('should return null for an invalid index position', () => {
+  // === sad path === //
+  it("should return null for an invalid index position", () => {
     const indexPosition = -1;
     const userData = getUserData(userSample.sampleUsers, indexPosition);
     expect(userData).to.equal(null);
   });
-// === sad path === //
-it('should return null for an empty user data array', () => {
-  const indexPosition = 0; 
-  const userData = getUserData([], indexPosition);
-  expect(userData).to.equal(null);
+  // === sad path === //
+  it("should return null for an empty user data array", () => {
+    const indexPosition = 0;
+    const userData = getUserData([], indexPosition);
+    expect(userData).to.equal(null);
+  });
 });
-});
-
