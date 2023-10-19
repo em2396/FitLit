77267f0c3d108cf03701dfba914389c93cbe53c1
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-10-18 20:01:48 -0400

    Update DOM to display sleep functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,7 +9,7 @@ const averageSleepOverall = document.querySelector('#averageSleepOverall');
 const sleepSpecificDay = document.querySelector('#sleepSpecificDay');
 const sleepPerWeek = document.querySelector('#sleepPerDayPerWeek')
 
-export function displayUserInfo(currentUser, averageSteps, waterPerSpecificDay, waterPerDayPerWeek, averageSleep, aveSleepQuality, sleepPerDayPerWeek, todaysHydrationDate) {
+export function displayUserInfo(currentUser, averageSteps, waterPerSpecificDay, waterPerDayPerWeek, averageSleep, aveSleepQuality, sleepPerDayPerWeek, todaysHydrationDate, sleepToday) {
   userName.innerText = `${currentUser.name}`;
   location.innerText = `${currentUser.address}`;
   welcomeBack.innerText = `Welcome Back, ${currentUser.firstName}!`;
@@ -24,9 +24,10 @@ export function displayUserInfo(currentUser, averageSteps, waterPerSpecificDay, 
 
   // As a user, I should be able to see my sleep data over the course of the latest week (hours slept and quality of sleep)
   sleepPerDayPerWeek.forEach((data) => {
-    sleepPerWeek.innerHTML += `<p> On ${data.date}, user slept for ${data.hoursSlept} hours with sleep quality of ${data.sleepQuality}`
+    console.log("Data", data)
+    sleepPerWeek.innerHTML += `<p> On ${data.date}, user slept for ${data.hoursSlept} hours with sleep quality of ${data.sleepQuality}`;
   });
-  sleepSpecificDay.innerText = hoursSlept;
+  sleepSpecificDay.innerHTML += `<p> Today (${sleepToday.date}), user slept for ${sleepToday.hoursSlept} hours with sleep quality of ${sleepToday.sleepQuality}`;
 
   //  should be able to see my sleep data for the latest day (hours slept and quality of sleep)
   //NEXT STEP: have to create a function to find the sleep data for latest day!
