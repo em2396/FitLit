6469701af325a248e210f74aa820dcdbd16ed191
Mary Bruff <marybruff5@gmail.com>
2023-11-01 05:42:16 -0600

    convert fectchPosts to es6

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,16 @@
+// // export const urls = [
+// //   "https://fitlit-api.herokuapp.com/api/v1/users",
+// //   "https://fitlit-api.herokuapp.com/api/v1/sleep",
+// //   "https://fitlit-api.herokuapp.com/api/v1/hydration",
+// //   "https://fitlit-api.herokuapp.com/api/v1/activity",
+// // ];
+// // ^^^^This is the old data set from Part 1^^^^^
+
 export const urls = [
-  "https://fitlit-api.herokuapp.com/api/v1/users",
-  "https://fitlit-api.herokuapp.com/api/v1/sleep",
-  "https://fitlit-api.herokuapp.com/api/v1/hydration",
-  "https://fitlit-api.herokuapp.com/api/v1/activity",
+  "http://localhost:3001/api/v1/users",
+  "http://localhost:3001/api/v1/sleep",
+  "http://localhost:3001/api/v1/activity",
+  "http://localhost:3001/api/v1/hydration"
 ];
 
 export const fetchPromises = urls.map(url =>
@@ -10,5 +18,23 @@ export const fetchPromises = urls.map(url =>
     .then((response) => response.json())
     .then((data) => {
       return data;
+    }).catch(error => {
+      console.log(error)
     })
 );
+
+
+//if input boxes are both filled, then invoke fetch 
+export const fetchPosts = (data) => {
+  fetch ('http://localhost:3001/api/v1/hydration', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then (response => response.json())
+  .then (json => console.log(json, 'json'))
+  .catch (error => console.log(error))
+}
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,38 +1,47 @@
-import { getRandomUser, getUserData, filterUserData, getMilesPerDay, getMinutesPerDay, getStepGoal, compareStepGoal, universalAverage, getLatestData, getInfoPerDay } from './data-model.js';
+import { getRandomUser, getUserData, filterUserData, getMilesPerDay, getMinutesPerDay, getStepGoal, theWaterChart, theStepChart, theActivityChart, theSleepingChart, compareStepGoal, universalAverage, getLatestData, getInfoPerDay, sendDataToAPI } from './data-model.js';
 // import {  } from './hydrationFunctions.js';
 import { theWaterChart, theStepChart, theActivityChart, theSleepingChart } from './charts.js'
 
 
 import { displayUserInfo, displayWaterInfo, displaySleepInfo, displayActivityInfo, displayStepInfo } from './domUpdates.js';
-import { fetchPromises } from './apiCalls.js';
+import { fetchPromises, fetchPosts } from './apiCalls.js';
 import './styles.css';
 
 //QuerySelectors Here:
 const userToggleButton = document.querySelector('.toggleButton');
 const userInformation = document.querySelector('.user-info');
+const addButton = document.querySelector('#addButton');
+const dateInput = document.querySelector('#dateInput');
+const ouncesInput = document.querySelector('#ouncesInput');
 
 //Variables Here:
 let userDataAll;
 let sleepDataAll;
 let hydrationDataAll;
 let activityDataAll;
+// let hydrationData;
+let currentUser
+
 
 //Event Listeners Here:
 window.addEventListener('load', function () {
   Promise.all(fetchPromises).then((values) => {
     //data from Web APIs:
+    // console.log(values, 'API values')
     userDataAll = values[0].users;
     sleepDataAll = values[1].sleepData;
-    hydrationDataAll = values[2].hydrationData;
-    activityDataAll = values[3].activityData;
-
+    activityDataAll = values[2].activityData;
+    hydrationDataAll = values[3].hydrationData;
+    
     //random currentUser functions:
     let randomUserIndex = getRandomUser(userDataAll);
-    let currentUser = getUserData(userDataAll, randomUserIndex);
- 
+    currentUser = getUserData(userDataAll, randomUserIndex);
+    console.log(currentUser, 'current')
+    
     //Hydration functions:
     let hydrationData = filterUserData(hydrationDataAll, currentUser);  
     let todaysHydrationDate = getLatestData(hydrationData);
+    // console.log(todaysHydrationDate, 'today')
     let waterPerDayPerWeek = getLatestData(hydrationData, 'week');
     let waterChartToDom = theWaterChart(waterPerDayPerWeek);
     
@@ -70,4 +79,11 @@ window.addEventListener('load', function () {
 userToggleButton.addEventListener('click',function() {
   userInformation.classList.toggle('hidden')
 });
+
+//Create an event Listener for when the button is added. This will invoke the function fetchPost which will send a post request to update data info
+addButton.addEventListener('click', function(event) {
+  event.preventDefault()
+  sendDataToAPI(currentUser)
+  setTimeout(console.log(userDataAll), 2000)
+})
 
