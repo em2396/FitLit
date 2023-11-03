aa29737efdcd139d4bac241d27854a429cc22a17
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-11-03 17:18:57 -0400

    Feat/clean up code

--- src/data-model.js
+++ src/data-model.js
@@ -166,11 +166,9 @@ interact(targetElement)
 
   export function dragMoveListener (event) {
     var target = event.target
-    // keep the dragged position in the data-x/data-y attributes
     var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
     var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
   
-    // translate the element
     target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
     target.style.cursor = 'grabbing';
 
